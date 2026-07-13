e77c3f0664f3adc30d18dc53475b2901b9e1b7b8
Yagy Deshmukh <yagy.deshmukh@gmail.com>
2026-07-13 21:17:15 +0530

    Update __init__.py

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,1 +1,13 @@
-# Package initialization
+"""
+routers/__init__.py
+
+Marks the 'routers' directory as a Python package.
+
+This file also exposes all API router modules,
+making imports cleaner throughout the project.
+"""
+
+from . import resume
+from . import roadmap
+from . import interview
+from . import cover_letter
